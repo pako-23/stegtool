bb01974e2ab893e687ff95a8173617639ac6039b
malynkz <mattia.malinconico@gmail.com>
2025-09-19 14:18:17 +0200

    Argument parsing in main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, const char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, const char *argv[]) 
+{ 
+	if (argc != 2)
+		return 1;
+	
+	const char *filename = argv[1];
+
+	FILE *fp = fopen(filename, "rb+");
+	if (!fp) 
+		return 1;
+}
