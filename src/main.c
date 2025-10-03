834c33558ff9b8309dd9f84679d1a3d313a6bc98
malynkz <mattia.malinconico@gmail.com>
2025-10-03 14:53:13 +0200

    flag parsing

--- src/main.c
+++ src/main.c
@@ -2,12 +2,55 @@
 #include <stdio.h>
 #include <stegtool.h>
 
+#define HELP_FMT \
+"help page %s\n"
+
 int main(int argc, const char *argv[]) 
 { 
-	const char *img = NULL;
 	const char *msg = NULL;
+	const char* img = NULL;
+	const char* output = NULL;
+	int decode = 0;
+	
+	int i = 1;
+	while (i < argc) {
+		if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--decode") == 0) {
+			if (decode) {
+				fprintf(stderr, "flag found two or more times: %s\n", argv[i]);
+				return EXIT_FAILURE;
+			}
 
-	const char *output; 
+			decode = 1;
+		} else if (strcmp(argv[i], "-m") == 0 || strcmp(argv[i], "--message") == 0) {
+			if (msg != NULL) {
+				fprintf(stderr, "Message found two or more times.\n");
+				return EXIT_FAILURE;
+			} else if (i + 1 > argc - 1) {
+				fprintf(stderr, "Could not find message.\n");
+				return EXIT_FAILURE;
+			}
+			msg = argv[i + 1];
+			++i;
+		} else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+			printf(HELP_FMT, argv[0]);
+			return EXIT_SUCCESS;
+		} else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0) {
+			if (output != NULL) {
+				fprintf(stderr, "Output error.\n");
+				return EXIT_FAILURE;
+			} else if (i + 1 > argc - 1) {
+				fprintf(stderr, "Could not find output.\n");
+				return EXIT_FAILURE;
+			}
+			output = argv[i + 1];
+			++i;
+		} else {
+			fprintf(stderr, "Unrecognized flag: %s\n", argv[i]);
+			frpintf(stderr, HELP_FMT, argv[0]);
+			return EXIT_FAILURE;
+		}
+		++i;
+	}
 
 
 	if (strcmp(argv[2], "-h") == 0) {
@@ -30,6 +73,6 @@ int main(int argc, const char *argv[])
 	}
 
 	else if (strcmp(argv[2], "-o") == 0) {
-		printf("Output filename: %s\n", o_filename);
+		printf("Output filename: \n");
 	}
 }
