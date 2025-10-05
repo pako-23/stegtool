b72a637a288b8c5d62f61e6b6fcac732e1cd44c4
pako-23 <pasqualepolverino98@gmail.com>
2025-10-05 21:34:01 +0200

    refactor zlib/libpng builds

---
+++ cmake/libpng.cmake
@@ -0,0 +1,15 @@
+include(zlib)
+
+FetchContent_Declare(
+  libpng
+  GIT_REPOSITORY https://github.com/pnggroup/libpng
+  GIT_TAG        v1.6.50
+)
+
+set(PNG_SHARED OFF CACHE BOOL "" FORCE)
+set(PNG_TESTS OFF CACHE BOOL "" FORCE)
+set(SKIP_INSTALL_CONFIG_FILE ON CACHE BOOL "" FORCE)
+set(SKIP_INSTALL_ALL ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(libpng)
+
+set_target_properties(png_static PROPERTIES POSITION_INDEPENDENT_CODE ON)
