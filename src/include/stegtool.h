3e6c7dd906a2e19eda4bc0f6ef797bcf81b6393a
pako-23 <pasqualepolverino98@gmail.com>
2025-10-09 08:56:31 +0200

    add linting stage to the pipeline

--- src/include/stegtool.h
+++ src/include/stegtool.h
@@ -6,7 +6,6 @@
 #include <img/img.h>
 
 int sg_embed(struct img_s *img, const char *msg, size_t len);
-
 size_t sg_extract_len(const struct img_s *img);
 int sg_extract_msg(const struct img_s *img, char *msg);
 
