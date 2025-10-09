269b19a5c68bebecb0726c0f551a3169e1d16024
pako-23 <pasqualepolverino98@gmail.com>
2025-10-09 07:58:30 +0200

    cleanup main implementation

--- src/main.c
+++ src/main.c
@@ -1,32 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <stegtool.h>
 
-#define HELP_FMT \
-"help page %s\n"
+#define HELP_FMT                                                               \
+  "Usage: %s [options] <filename>\n\n"                                         \
+  "Options:\n"                                                                 \
+  "  -d, --decode\n"                                                           \
+  "       Run in decode mode. Extract data embedded into an image.\n"          \
+  "  -m <message>, --message <message>\n"                                      \
+  "       The message to embedd within the image. (required)\n"                \
+  "  -o <filename>, --output <filename>\n"                                     \
+  "       Write the program output to the given file. (default: stdout)\n"     \
+  "  -h, --help\n"                                                             \
+  "       Display this help and exit\n"
+
+
 
 int main(int argc, const char *argv[]) 
 { 
 	const char *msg = NULL;
 	const char* img = NULL;
 	const char* output = NULL;
-	int decode = 0;
-	
+    int decode = 0;
+
+    if (argc < 2) {
+        fprintf(stderr, "Missing input image\n");
+        fprintf(stderr, HELP_FMT, argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    img = argv[argc-1];
 	int i = 1;
-	while (i < argc) {
+	while (i < argc -1) {
 		if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--decode") == 0) {
 			if (decode) {
-				fprintf(stderr, "flag found two or more times: %s\n", argv[i]);
+				fprintf(stderr, "Flag specified multiple times: %s\n", argv[i]);
 				return EXIT_FAILURE;
 			}
 
 			decode = 1;
 		} else if (strcmp(argv[i], "-m") == 0 || strcmp(argv[i], "--message") == 0) {
 			if (msg != NULL) {
-				fprintf(stderr, "Message found two or more times.\n");
+				fprintf(stderr, "Flag specified multiple times: %s\n", argv[i]);
 				return EXIT_FAILURE;
-			} else if (i + 1 > argc - 1) {
-				fprintf(stderr, "Could not find message.\n");
+			} else if (i + 1 >= argc - 1) {
+				fprintf(stderr, "Flag requires argument: %s\n", argv[i]);
 				return EXIT_FAILURE;
 			}
 			msg = argv[i + 1];
@@ -36,43 +55,19 @@ int main(int argc, const char *argv[])
 			return EXIT_SUCCESS;
 		} else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0) {
 			if (output != NULL) {
-				fprintf(stderr, "Output error.\n");
+			    fprintf(stderr, "Flag specified multiple times: %s\n", argv[i]);
 				return EXIT_FAILURE;
-			} else if (i + 1 > argc - 1) {
-				fprintf(stderr, "Could not find output.\n");
+			} else if (i + 1 >= argc - 1) {
+				fprintf(stderr, "Flag requires argument: %s\n", argv[i]);
 				return EXIT_FAILURE;
 			}
 			output = argv[i + 1];
 			++i;
 		} else {
 			fprintf(stderr, "Unrecognized flag: %s\n", argv[i]);
-			frpintf(stderr, HELP_FMT, argv[0]);
+			fprintf(stderr, HELP_FMT, argv[0]);
 			return EXIT_FAILURE;
 		}
 		++i;
-	}
-
-
-	if (strcmp(argv[2], "-h") == 0) {
-		printf("Help page\n");
-
-	}
-
-	else if (strcmp(argv[2], "-m") == 0) {
-		printf("Hide message page\n");
-
-		const char *msg = argv[3];
-		size_t len = strlen(msg);
-
-		//img to be assigned
-		int sg_embed(img, msg, len);
-	}
-
-	else if (strcmp(argv[2], "-d") == 0) {
-		printf("Extract message page\n");
-	}
-
-	else if (strcmp(argv[2], "-o") == 0) {
-		printf("Output filename: \n");
-	}
+    }
 }
